5d71731110b404ded924f39b000c3de33cf5a0fb
zhang13pro <lexzhang13@qq.com>
2022-02-19 21:58:31 +0800

    update: component

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1 @click="toggle">click</h1>
+  <h1 @click="toggle">全屏</h1>
+  <Rate value="1" theme="yellow" />
 </template>
 <script setup>
+import Rate from "../components/Rate.vue";
 import { useFullscreen } from "@vueuse/core";
+
 const { isFullscreen, enter, exit, toggle } = useFullscreen();
 </script>
