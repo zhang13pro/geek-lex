0a370c62f25f50517abf584267f0644b87e90152
zhang13pro <lexzhang13@qq.com>
2022-02-19 21:37:38 +0800

    feature: init program

---
+++ src/components/ChangeFavicon.vue
@@ -0,0 +1,11 @@
+<script setup>
+import useFavicon from "./utils/favicon";
+let { favicon } = useFavicon();
+function loading() {
+  favicon.value = "/geek.png";
+}
+</script>
+
+<template>
+  <button @click="loading">123</button>
+</template>
